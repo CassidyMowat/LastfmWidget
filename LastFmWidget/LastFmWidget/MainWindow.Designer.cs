4f1587cd1600dbe17c5476bd26ebe003ff253b1b
CassidyMowat <cassidy-35@hotmail.com>
2016-11-18 15:50:11 +1300

    Added method for initial api call

--- LastFmWidget/LastFmWidget/MainWindow.Designer.cs
+++ LastFmWidget/LastFmWidget/MainWindow.Designer.cs
@@ -23,24 +23,49 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.tbUserName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.btnLoad = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // tbUserName
+            // 
+            this.tbUserName.Location = new System.Drawing.Point(78, 3);
+            this.tbUserName.Name = "tbUserName";
+            this.tbUserName.Size = new System.Drawing.Size(158, 20);
+            this.tbUserName.TabIndex = 0;
+            this.tbUserName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(110, 106);
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.label1.Location = new System.Drawing.Point(12, 6);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(40, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "memes";
+            this.label1.Size = new System.Drawing.Size(60, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "User Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(242, 1);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(59, 23);
+            this.btnLoad.TabIndex = 4;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnContinue_Click);
             // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(668, 858);
+            this.Controls.Add(this.btnLoad);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbUserName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "MainWindow";
             this.Text = "Last.fm Widget";
             this.ResumeLayout(false);
@@ -50,6 +75,9 @@
 
         #endregion
 
+        private System.Windows.Forms.TextBox tbUserName;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLoad;
     }
 }
+
--- LastFmWidget/LastFmWidget/MainWindow.cs
+++ LastFmWidget/LastFmWidget/MainWindow.cs
@@ -3,15 +3,62 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace LastFmWidget {
-    public partial class MainWindow : Form {
-        public MainWindow() {
+namespace LastFmWidget{
+
+    public partial class MainWindow : Form{
+
+        private string apiKey = "773fbc4130ba98c24aff91aceca3d25b";
+        private string userName = "";
+
+        public MainWindow(){
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e){
+
+        }
+
+
+        private void label1_Click(object sender, EventArgs e){
+
+        }
+
+        private void btnContinue_Click(object sender, EventArgs e){
+
+            if(tbUserName.Text.Length > 0){
+                userName = tbUserName.Text;
+                Console.WriteLine(userName);
+
+                getRecentTracks();
+            }
+            else {
+                Console.WriteLine("Username Empty");
+            }
+
+        }
+
+
+        private void getRecentTracks() {
+
+            String url = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + userName + "&api_key=" + apiKey + "&format=json";
+
+            try {
+                HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+                Stream resStream = response.GetResponseStream();
+                Console.WriteLine(resStream.ToString());
+
+            } catch(Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+
     }
 }
